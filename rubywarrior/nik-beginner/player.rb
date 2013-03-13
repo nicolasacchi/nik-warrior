af27b324575874b9bc1f2e0f2cfa33896b080f22
Nicola Sacchi <sacchi.nicola@gmail.com>
2013-03-13 18:18:32 +0100

    9

--- rubywarrior/nik-beginner/player.rb
+++ rubywarrior/nik-beginner/player.rb
@@ -5,39 +5,69 @@ class Player
     @toccato_il_muro ||= false
     @toccato_il_muro = true if warrior.feel(:backward).wall?
     dont_rest = warrior.health < @health
-          
-    #check look
-    look = warrior.look
-    if look[0].enemy?
-      if warrior.health < 15 and !dont_rest
-        warrior.walk!(:backward)
-      else
-        warrior.attack!
+#p warrior.look[1].to_s
+
+    if warrior.enemy_vicini?
+      dir,pos = warrior.where_enemy?
+      case pos
+        when 0
+          if warrior.health < 15 and !dont_rest
+            warrior.walk!(inverse_of(dir))
+          else
+            warrior.attack!(dir)
+          end
+        when 1,2
+          warrior.shoot!(dir)
       end
     else
-      if look[1].enemy? or (look[1].empty? and look[2].enemy?)
-        warrior.shoot!
-      elsif warrior.health != 20 and !dont_rest
-        warrior.rest!
-      elsif warrior.health < 15
-        warrior.walk!(:backward)
+      if warrior.see_archer?
+        dir,pos = warrior.see_archer?
+        warrior.shoot!(dir)
       else
-        if warrior.captive_to_rescue?
-          warrior.rescue!(warrior.captive_to_rescue?)
-        else
-          if warrior.feel.wall?
-            warrior.pivot!
-          else
-            if !@toccato_il_muro
-              warrior.walk!(:backward)
+        if warrior.where_enemy?
+          dir,pos = warrior.where_enemy?
+          case pos
+          when 0
+            if warrior.health < 15 and !dont_rest
+              warrior.walk!(inverse_of(dir))
             else
-              warrior.walk!
+              warrior.attack!(dir)
+            end
+          when 1,2
+            warrior.shoot!(dir)
+          end
+        else
+          if warrior.health != 20 and !dont_rest
+            warrior.rest!
+          elsif warrior.health < 15
+            warrior.walk!(:backward)
+          else
+            if warrior.captive_to_rescue?
+              warrior.rescue!(warrior.captive_to_rescue?)
+            else
+              if warrior.feel.wall?
+                warrior.pivot!
+              else
+                if !@toccato_il_muro
+                  warrior.walk!(:backward)
+                else
+                  warrior.walk!
+                end
+              end
             end
           end
         end
       end
     end
     @health = warrior.health
+  end
+  def inverse_of(dir)
+    case dir
+    when :backward
+      return :forward
+    when :forward
+      return :backward
+    end
   end
 end
 class RubyWarrior::Turn
@@ -49,5 +79,29 @@ class RubyWarrior::Turn
       return dir if self.feel(dir).captive?
     end
     return false
+  end
+  def where_enemy?
+    (0..2).each do |pos|
+      [:forward, :backward].each do |dir|
+        if self.look(dir)[pos].enemy?
+          return dir,pos
+        end
+      end
+    end
+    return nil
+  end
+  def see_archer?
+    (0..2).each do |pos|
+      [:forward, :backward].each do |dir|
+        if self.look(dir)[pos].to_s == "Archer"
+          return dir,pos
+        end
+      end
+    end
+    return nil
+  end
+  def enemy_vicini?
+    dir,pos = self.where_enemy?
+    return pos == 0
   end
 end
