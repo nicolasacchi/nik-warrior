d16c8395a0dc7b6ebcab37018ede7d454b05764c
Nicola Sacchi <sacchi.nicola@gmail.com>
2013-03-13 17:18:07 +0100

    5

--- rubywarrior/nik-beginner/player.rb
+++ rubywarrior/nik-beginner/player.rb
@@ -2,20 +2,43 @@ class Player
   def play_turn(warrior)
     # add your code here
     @health ||= warrior.health
+    @toccato_il_muro ||= false
+    @toccato_il_muro = true if warrior.feel(:backward).wall?
     dont_rest = warrior.health < @health
     if warrior.feel.enemy?
-      warrior.attack!
+      if warrior.health < 15 and !dont_rest
+        warrior.walk!(:backward)
+      else
+        warrior.attack!
+      end
     else
       if warrior.health != 20 and !dont_rest
         warrior.rest!
+      elsif warrior.health < 15
+        warrior.walk!(:backward)
       else
-        if warrior.feel.captive?
-          warrior.rescue!
+        if warrior.captive_to_rescue?
+          warrior.rescue!(warrior.captive_to_rescue?)
         else
-          warrior.walk!
+          if !@toccato_il_muro
+            warrior.walk!(:backward)
+          else
+            warrior.walk!
+          end
         end
       end
     end
     @health = warrior.health
+  end
+end
+class RubyWarrior::Turn
+  def directions
+    [:forward, :backward, :right, :left]
+  end
+  def captive_to_rescue?
+    directions.each do |dir|
+      return dir if self.feel(dir).captive?
+    end
+    return false
   end
 end
