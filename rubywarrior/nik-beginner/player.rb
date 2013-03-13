608995666328ffd3143fc00e1f1eb09a7a175297
Nicola Sacchi <sacchi.nicola@gmail.com>
2013-03-13 16:50:12 +0100

    4

--- rubywarrior/nik-beginner/player.rb
+++ rubywarrior/nik-beginner/player.rb
@@ -1,14 +1,21 @@
 class Player
   def play_turn(warrior)
     # add your code here
+    @health ||= warrior.health
+    dont_rest = warrior.health < @health
     if warrior.feel.enemy?
-      warrior.attack!
+#      if warrior.health < 5
+#        warrior.walk!(:backward)
+#      else
+        warrior.attack!
+#      end
     else
-      if warrior.health != 20
+      if warrior.health != 20 and !dont_rest
         warrior.rest!
       else
         warrior.walk!
       end
     end
+    @health = warrior.health
   end
 end
